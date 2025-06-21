a8aa5976bd0a858fa3d660691ac50aa63f922810
Julian Winter <julian.winter91@gmail.com>
2025-06-21 19:44:40 +0200

    Add ipc handler for renaming files

--- ipcHandlers.js
+++ ipcHandlers.js
@@ -1,8 +1,11 @@
 const { ipcMain, dialog } = require('electron');
 const { getMainWindow } = require('./windowManager');
 const { extendFileInformation } = require('./fileUtils');
+const fs = require('fs');
+const path = require('path');
 
 ipcMain.handle('dialog:openFiles', async () => {
+    console.log('dialog.showOpenDialog()');
     try {
         const result = await dialog.showOpenDialog(getMainWindow(), {
             properties: ['openFile', 'multiSelections'],
@@ -20,4 +23,26 @@ ipcMain.handle('dialog:openFiles', async () => {
         console.error('Fehler beim Öffnen des Dialogs:', error);
         throw new Error('Fehler beim Öffnen des Dateidialogs.');
     }
+});
+
+ipcMain.handle('files:renameFiles', async (event, filesToRename) => {
+    const errors = [];
+
+    for (const file of filesToRename) {
+        console.log(file);
+        try {
+            const oldPath = path.join(file.path, file.name + file.extension);
+            const newPath = path.join(file.path, file.changedName + file.extension);
+            console.log(oldPath);
+            console.log(newPath);
+            await fs.promises.rename(oldPath, newPath);
+        } catch (error) {
+            errors.push({ file: file.name, message: error.message });
+        }
+    }
+
+    if (errors.length > 0) {
+        return { success: false, errors };
+    }
+    return { success: true };
 });
--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // Exponiere eine API für den Renderer-Prozess
 contextBridge.exposeInMainWorld('electron', {
-  openFiles: () => ipcRenderer.invoke('dialog:openFiles')
+  openFiles: () => ipcRenderer.invoke('dialog:openFiles'),
+  renameFiles: (filesToRename) => ipcRenderer.invoke('files:renameFiles', filesToRename)
 });
